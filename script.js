205e9f0f4d504b547bd89395952b7b1649819490
= <aminicomputer96@gmail.com>
2021-06-04 11:45:07 +0430

    Hide modal on outside click

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ open.addEventListener('click', () => modal.classList.add('show-modal'));
 // Hide modal
 close.addEventListener('click', () => modal.classList.remove('show-modal'));
 
+// Hide modal on outside click
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
