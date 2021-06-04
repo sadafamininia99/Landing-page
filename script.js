1af531e3ace55794ec43bd74977009de1ebde03e
= <aminicomputer96@gmail.com>
2021-06-04 11:38:43 +0430

    close btn

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const toggle = document.getElementById('tiggle');
+ 
+const toggle = document.getElementById('toggle');
 const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 
-
-//toggle navigation
-toggle.addEventListener('clicl',() =>
-document.body.classList.toggle('show-nav')
-);  
+// Toggle nav
+toggle.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav')
+);
