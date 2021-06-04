9ad4af60ca858d1e59780962c1006bcb41e864e4
= <aminicomputer96@gmail.com>
2021-06-04 11:30:01 +0430

    add function for button

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById('tiggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+//toggle navigation
+toggle.addEventListener('clicl',() =>
+document.body.classList.toggle('show-nav')
+);  
